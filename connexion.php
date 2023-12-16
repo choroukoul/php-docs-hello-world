e42e67080a61d521bdbae8da4b232eddc9359e28
fatiuser <fatiuser>
2023-12-17 00:42:56 +0100

    jjj

--- connexion.php
+++ connexion.php
@@ -4,7 +4,11 @@ try {
     $database = "rdv";
     $authentication = "Active Directory Default";  // Specify the authentication method
 
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database;Authentication=$authentication", null, null);
+    $conn = new PDO(
+        "sqlsrv:server = tcp:rdvserveur.database.windows.net,1433; Database = rdv",
+       "chorouk.oulahyane@uir.ac.ma",
+        "Loulouk002"
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
