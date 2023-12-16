94decc7f55c59879c11a201626cf84d6b855b470
fatiuser <fatiuser>
2023-12-17 00:37:10 +0100

    hhhh

--- connexion.php
+++ connexion.php
@@ -1,10 +1,13 @@
 <?php
 try {
-    // Establish the connection
-    $conn = new PDO("sqlsrv:server = tcp:rdvserveur.database.windows.net,1433; Database = rdv", "hopital", "Vos51627");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $serverName = "tcp:rdvserveur.database.windows.net,1433";
+    $database = "rdv";
+    $authentication = "Active Directory Default";  // Specify the authentication method
 
-} catch (PDOException $e) {
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database;Authentication=$authentication", null, null);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
