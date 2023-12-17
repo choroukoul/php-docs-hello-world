4a360d782c4e7ec8c166875ff015469df2f39962
fatiuser <fatiuser>
2023-12-17 12:14:18 +0100

    hhhhhh

--- connexion.php
+++ connexion.php
@@ -1,18 +1,31 @@
 <?php
 try {
+    // Specify the Azure AD authentication details
     $serverName = "tcp:rdvserveur.database.windows.net,1433";
     $database = "rdv";
-    $authentication = "Active Directory Default";  // Specify the authentication method
+    $clientId = "30912fd0-28ef-450c-a43a-c6902b6883a5";
+    $clientSecret = "d2d26786-82fa-4764-9aac-f8d2ef7165be";
+    $tenantId = "3bd72a86-a8ea-44a6-a899-f3cccbedf027";
+    $username = "chorouk.oulahyane@uir.ac.ma";
+    $password = "Loulouk002"; // The user's password
 
+    // Construct the connection string with Azure AD authentication
     $conn = new PDO(
-        "sqlsrv:server = tcp:rdvserveur.database.windows.net,1433; Database = rdv",
-       "chorouk.oulahyane@uir.ac.ma",
-        "Loulouk002"
+        "sqlsrv:server=$serverName;Database=$database;",
+        NULL,
+        NULL,
+        [
+            PDO::SQLSRV_ATTR_AUTHENTICATION => PDO::SQLSRV_AUTH_AZURE_AD,
+            PDO::SQLSRV_ATTR_CLIENT_ID => $clientId,
+            PDO::SQLSRV_ATTR_CLIENT_SECRET => $clientSecret,
+            PDO::SQLSRV_ATTR_TENANT_ID => $tenantId,
+        ]
     );
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 ?>
+
