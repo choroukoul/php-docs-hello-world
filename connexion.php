acca249851a0e6aeaab4afb682bb0b9ac67faa06
fatiuser <fatiuser>
2023-12-17 12:54:58 +0100

    ooo

--- connexion.php
+++ connexion.php
@@ -3,17 +3,19 @@ try {
     // Specify the connection details
     $serverName = "tcp:rdvserveur.database.windows.net,1433";
     $database = "rdv";
-    $username = "chorouk.oulahyane@uir.ac.ma"; // This should be the Windows username (e.g., domain\username) or a SQL Server login if needed
-    //$password = "{your_password}"; // This is optional for Windows authentication
+    $username = "chorouk.oulahyane@uir.ac.ma";
+    $password = NULL;
 
     // Construct the connection string with Active Directory Integrated Authentication
     $conn = new PDO(
         "sqlsrv:server=$serverName;Database=$database;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=ActiveDirectoryIntegrated;",
         $username,
-        $password
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::SQLSRV_ATTR_QUERY_TIMEOUT => 30, // Set query timeout if needed
+        ]
     );
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
