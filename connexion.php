629a4b468696cbd26246acc0094639a9b7c9685b
fatiuser <fatiuser>
2023-12-17 15:23:23 +0100

    jjjj

--- connexion.php
+++ connexion.php
@@ -1,21 +1,9 @@
 <?php
 try {
-    // Specify the connection details
-    $serverName = "tcp:rdvserveur.database.windows.net,1433";
-    $database = "rdv";
-    $username = "chorouk.oulahyane@uir.ac.ma";
-    $password = NULL;
+    // Establish the connection
+    $conn = new PDO("sqlsrv:server = tcp:yousraserveur.database.windows.net,1433; Database = yousrabdd", "yousraadmin", "Yousramdp123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Construct the connection string with Active Directory Integrated Authentication
-    $conn = new PDO(
-        "sqlsrv:server=$serverName;Database=$database;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=ActiveDirectoryIntegrated;",
-        $username,
-        $password,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::SQLSRV_ATTR_QUERY_TIMEOUT => 80, // Set query timeout if needed
-        ]
-    );
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réservation de Salles - Connexion</title>
-    <link rel="stylesheet" href="styleAuthenti.css">
+    <link rel="stylesheet" href="styleindex.css">
 </head>
 <body>
-    <form action="logicLogin.php" method="post">
+    <form action="indexWork.php" method="post">
         <h2>Connexion</h2>
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" required>
